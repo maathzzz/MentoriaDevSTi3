e485d12d775af6ce1c61dde67a2dc7fa76ff5b1c
Matheus <matheusdeamorim20@gmail.com>
2022-11-29 12:29:57 -0300

    tela produtos

---
+++ MentoriaDevSTi3/View/UserControls/UcProdutos.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MentoriaDevSTi3.View.UserControls
+{
+    /// <summary>
+    /// Interação lógica para UcProdutos.xam
+    /// </summary>
+    public partial class UcProdutos : UserControl
+    {
+        public UcProdutos()
+        {
+            InitializeComponent();
+        }
+    }
+}
